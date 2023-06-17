e136bd8ccac8449f4bc05bdeea3cf4a65c757595
Rui Costa <ruicosta042@gmail.com>
2023-06-17 23:00:16 +0100

    chore: update deps, security fixes

--- sh/build.ts
+++ sh/build.ts
@@ -1,10 +1,12 @@
-import "zx/globals";
+import * as shell from "@tuplo/shell";
 
 async function main() {
+	const $ = shell.$({ verbose: true });
+
 	await $`rm -rf dist`;
 	await $`tsc --build tsconfig.build.json`;
 
-	const flags = ["--bundle", "--platform=node", "--minify"];
+	const flags = ["--bundle", "--platform=node"];
 
 	await $`esbuild src/cjs/index.cjs --outfile=dist/index.cjs ${flags}`;
 	await $`esbuild src/index.ts --format=esm --outfile=dist/index.mjs ${flags}`;
--- src/mocks/helpers/request.ts
+++ src/mocks/helpers/request.ts
@@ -1,10 +1,10 @@
-import type { RestRequest, DefaultBodyType } from "msw";
+import type { DefaultBodyType, RestRequest } from "msw";
 
 type Request = RestRequest<DefaultBodyType>;
 
 type Headers = Record<string, string>;
 export function getHeaders(req: Request) {
-	return [...req.headers.entries()].reduce((acc, [key, value]) => {
+	return Array.from(req.headers.entries()).reduce((acc, [key, value]) => {
 		if (key === "x-msw-request-id") return acc;
 		if (value === "") return acc;
 
@@ -17,7 +17,7 @@ export function getQuery(req: Request) {
 	const { url } = req;
 	const { searchParams } = url as URL;
 
-	return [...searchParams.entries()].reduce((acc, [key, value]) => {
+	return Array.from(searchParams.entries()).reduce((acc, [key, value]) => {
 		acc[key] = value;
 		return acc;
 	}, {} as Record<string, string>);
